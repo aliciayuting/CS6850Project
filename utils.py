e6c7cc7e4b355d13d09f080928226cd3ee0ed86d
Ben Landrum <8enlandrum@gmail.com>
2024-12-10 15:38:32 -0500

    3d plot

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 # %%
 import matplotlib.pyplot as plt
 import numpy as np
+import plotly.graph_objects as go
 
 def plot_2d_graph(points, neighborhoods, point_kwargs={}, edge_kwargs={}):
     """Plots a graph over 2d points. Accepts kwargs to be sent to the plotting
@@ -34,13 +35,62 @@ def plot_2d_graph(points, neighborhoods, point_kwargs={}, edge_kwargs={}):
     ax.axis('off')
 
     return fig
+
+def plot_3d_graph(points, neighborhoods, point_kwargs={}, edge_kwargs={}):
+    """Plots a graph over 3d points in 3d space, creating an interactive plotly
+    visualization. Works even in jupyter notebooks. Accepts kwargs to be sent 
+    to the plotting functions.
+
+    Args:
+        points (numpy.ndarray): 2d numpy array of the vectors of the points
+        neighborhoods (List[List]): A list of lists describing neighborhoods
+
+    Returns:
+        A figure containing the visualization
+    """
+    fig = go.Figure()
+
+    # add points
+    fig.add_trace(go.Scatter3d(
+        x=points[:, 0],
+        y=points[:, 1],
+        z=points[:, 2],
+        mode='markers',
+        **point_kwargs
+    ))
+
+    # add edges
+    for i, neighbors in enumerate(neighborhoods):
+        p1 = points[i]
+        for n in neighbors:
+            p2 = points[n]
+            fig.add_trace(go.Scatter3d(
+                x=[p1[0], p2[0]],
+                y=[p1[1], p2[1]],
+                z=[p1[2], p2[2]],
+                mode='lines',
+                **edge_kwargs
+            ))
+
+    fig.update_layout(
+        scene=dict(
+            xaxis=dict(visible=False),
+            yaxis=dict(visible=False),
+            zaxis=dict(visible=False)
+        ),
+        margin=dict(l=0, r=0, b=0, t=0),
+        showlegend=False
+    )
+
+    return fig
+
 # %%
 if __name__ == '__main__':
     # k-nn graph example
     rng = np.random.default_rng()
-    X = rng.normal(size=(50, 2))
+    X = rng.normal(size=(50, 3))
     edges = [np.argsort([np.linalg.norm(X[x] - X[i]) for i in range(50)])[1:6] for x in range(50)]
     
-    fig = plot_2d_graph(X, edges)
-    plt.show()
+    fig = plot_3d_graph(X, edges)
+    fig.show()
     
